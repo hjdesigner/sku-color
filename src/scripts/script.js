e2803d88a02decade9f4d6bd9acff96553ded60b
Henrique Rodrigues <henrique.rodrigues@profite.com.br>
2017-08-08 19:31:47 -0300

    Placed validation for the form and checked also if it is a hexadecimal color or image

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,6 +5,7 @@ var input = document.querySelector('[data-js="formUrl"]');
 var button = document.querySelector('[data-js="procurar"]');
 var viewDiv = document.querySelector('[data-js="colorView"]')
 var viewColor = document.querySelector('[data-js="color"]');
+var error = document.querySelector('[data-js="erro"]');
 var xhr = new XMLHttpRequest();
 var className;
 var classValue;
@@ -17,30 +18,40 @@ button.addEventListener('click', ajaxHtml)
 
 function ajaxHtml(){
     var url = input.value;
-    xhr.open('GET', url);
-    xhr.onreadystatechange = () => {
-        if(xhr.readyState === 4){
-            if(xhr.status === 200){
-                html = xhr.responseText;
-                readExcel(html);
-            }else{
-                console.log('deu merda');
+    if( url !== ''){
+        xhr.open('GET', url);
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState === 4){
+                if(xhr.status === 200){
+                    html = xhr.responseText;
+                    readExcel(html);
+                }else{
+                    console.log('deu merda');
+                }
             }
         }
+        xhr.send();
+    }else{
+        error.style.display = 'block';
     }
-    xhr.send();
 }
 
 function readExcel(cont){
+    error.style.display = 'none';
     var teste = cont;
     htmlCont.innerHTML = teste;
     var tr = document.querySelectorAll('tr');
     //console.log(tr);
     tr.forEach(function(el, i) {
-        className = el.children[1].textContent;
+        className = el.children[1].textContent.replace(' ','-');
         classValue = el.children[2].textContent;
-        cssColor = '.' + className + '{ background-color: #' + classValue + '} <br />';
-        viewHtml = '<li><div class="color-thumb" style="background-color: #'+ classValue +' "></div><span>'+ className +'</span></li>';
+        if(classValue.match(/#/g)){
+            cssColor = '.skuespec_Cores_opcao_' + className + '{ background-color: ' + classValue + '} <br />';
+            viewHtml = '<li><div class="color-thumb" style="background-color: '+ classValue +' "></div><span>'+ className +'</span></li>';
+        }else{
+            cssColor = '.skuespec_Cores_opcao_' + className + '{ background-image: url(' + classValue + ')} <br />';
+            viewHtml = '<li><div class="color-thumb" style="background-image: url('+ classValue +')"></div><span>'+ className +'</span></li>';
+        }
         cssFinish.insertAdjacentHTML('beforeend', cssColor);
         viewColor.insertAdjacentHTML('beforeend', viewHtml);
         cssFinish.style.opacity = '1';
