be3ca1b62173912a91f0cd1b400a980b7a0c89b7
Henrique Melanda <hjdesigner23@gmail.com>
2017-08-13 00:21:07 -0300

    Tornei o html mais semantico, incluir botões de compartilhamento, coloquei o codigo do analitcs, agora o projeto já mostra css e cores e no javascripts deixei ele mais limpo removendo algumas etapas e coloquei para montar o html completo dinamico das cores

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,71 +1,74 @@
-var htmlCont = document.querySelector('[data-js="htmlCont"]');
-var cssFinish = document.querySelector('[data-js="cssFinish"]');
-var input = document.querySelector('[data-js="formUrl"]');
-var button = document.querySelector('[data-js="procurar"]');
-var viewDiv = document.querySelector('[data-js="colorView"]')
-var viewColor = document.querySelector('[data-js="color"]');
-var error = document.querySelector('[data-js="erro"]');
-var clip = document.querySelector('[data-js="clip"]');
-var xhr = new XMLHttpRequest();
-var html;
-var className;
-var classValue;
-var cssColor;
-var sassColor;
-var url;
-var viewHtml;
+var input = document.querySelector('[data-js="formUrl"]'),
+    button = document.querySelector('[data-js="procurar"]'),
+    viewDiv = document.querySelector('[data-js="colorView"]'),
+    viewColor = document.querySelector('[data-js="color"]'),
+    error = document.querySelector('[data-js="erro"]'),
+    clip = document.querySelector('[data-js="clip"]'),
+    main = document.querySelector('[data-js="main"]'),
+    templateHTMLCSS = document.querySelector('[data-js="htmlCSS"]'),
+    viewCSS = document.querySelector('[data-js="cssFinish"]'),
+    xhr = new XMLHttpRequest(),
+    urlValidator = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g),
+    parser = new DOMParser(),
+    html,
+    className,
+    classValue,
+    cssColor,
+    url,
+    viewHtml,
+    table;
 
 button.addEventListener('click', ajaxHtml)
 
 
 function ajaxHtml(){
-    var url = input.value;
-    if( url !== ''){
-        xhr.open('GET', url);
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    html = xhr.responseText;
-                    readExcel(html);
-                }else{
-                    console.log('deu erro');
-                }
-            }
+  url = input.value;
+  if( url !== '' && url.match(urlValidator)){
+    xhr.open('GET', url);
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4){
+        if(xhr.status === 200){
+          html = xhr.responseText;
+          readExcel(html);
+        }else{
+          error.classList.add('active');
+          error.innerHTML = 'Não encontramos nenhuma tabela nessa url';
         }
-        xhr.send();
-    }else{
-        error.style.display = 'block';
+      }
     }
+    xhr.send();
+  }else{
+    error.classList.add('active');
+    error.innerHTML = 'Por favor coloque uma url válida';
+  }
 }
 
 function readExcel(cont){
-    error.style.display = 'none';
-    var teste = cont;
-    htmlCont.innerHTML = teste;
-    var tr = document.querySelectorAll('tr');
-    //console.log(tr);
-    tr.forEach(function(el, i) {
-        className = el.children[1].textContent.replace(' ','-');
-        classValue = el.children[2].textContent;
-        if(classValue.match(/#/g)){
-            //CSS
-            cssColor = '.skuespec_Cores_opcao_' + className + '{ background-color: ' + classValue + '} <br />';            
-            viewHtml = '<li><div class="color-thumb" style="background-color: '+ classValue +' "></div><span>'+ className +'</span></li>';
-        }else{
-            cssColor = '.skuespec_Cores_opcao_' + className + '{ background-image: url(' + classValue + ')} <br />';
-            viewHtml = '<li><div class="color-thumb" style="background-image: url('+ classValue +')"></div><span>'+ className +'</span></li>';
-        }
-        cssFinish.insertAdjacentHTML('beforeend', cssColor);
-        viewColor.insertAdjacentHTML('beforeend', viewHtml);
-        cssFinish.style.opacity = '1';
-        viewDiv.style.opacity = '1';
-    });
+  error.classList.remove('active');
+  clip.classList.remove('active');
+  table = parser.parseFromString(cont, "text/html").querySelectorAll('tr');
+  table.forEach(function(el, i) {
+    if( i != 0 ){
+      className = el.children[1].textContent.replace(' ','-');
+      classValue = el.children[2].textContent;
+      if(classValue.match(/#/g)){
+        cssColor = '.skuespec_Cores_opcao_' + className + '{ background-color: ' + classValue + '} <br />';            
+        viewHtml = '<li><div class="color-thumb" style="background-color: '+ classValue +' "></div><span>'+ className +'</span></li>';
+      }else{
+        cssColor = '.skuespec_Cores_opcao_' + className + '{ background-image: url(' + classValue + ')} <br />';
+        viewHtml = '<li><div class="color-thumb" style="background-image: url('+ classValue +')"></div><span>'+ className +'</span></li>';        
+      }
+      templateHTMLCSS.insertAdjacentHTML('beforeend', cssColor);
+      viewColor.insertAdjacentHTML('beforeend', viewHtml);
+    }    
+  });
+  viewCSS.insertAdjacentHTML('beforeend', templateHTMLCSS.textContent);
 }
 
 var clipboard = new Clipboard('[data-js="clip"]', {
-    text: function() {
-        return 'http://henriquemelanda.com.br/html-excel/cvs.html';
-    }
+  text: function() {
+    return 'http://henriquemelanda.com.br/html-excel/cvs.html';
+  }
 });
 
 clipboard.on('success', function(e) {
