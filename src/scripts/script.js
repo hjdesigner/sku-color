8ed647a04a8272fcb3eb04ca94cce5cb19262b99
Henrique Melanda <hjdesigner23@gmail.com>
2017-08-13 11:56:48 -0300

    Colocado loader, coloquei alguns comentarios no js para ajudar e feito uma validação para quando gerar um nova tabela a antiga some e mostra a nova

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,11 @@
 var input = document.querySelector('[data-js="formUrl"]'),
     button = document.querySelector('[data-js="procurar"]'),
-    viewDiv = document.querySelector('[data-js="colorView"]'),
     viewColor = document.querySelector('[data-js="color"]'),
     error = document.querySelector('[data-js="erro"]'),
     clip = document.querySelector('[data-js="clip"]'),
-    main = document.querySelector('[data-js="main"]'),
     templateHTMLCSS = document.querySelector('[data-js="htmlCSS"]'),
     viewCSS = document.querySelector('[data-js="cssFinish"]'),
+    loader = document.querySelector('[data-js="loader"]'),
     xhr = new XMLHttpRequest(),
     urlValidator = new RegExp(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g),
     parser = new DOMParser(),
@@ -26,11 +25,16 @@ function ajaxHtml(){
   if( url !== '' && url.match(urlValidator)){
     xhr.open('GET', url);
     xhr.onreadystatechange = () => {
+      if(xhr.readyState === 3){
+        loader.classList.add('is-active');
+      }
       if(xhr.readyState === 4){
         if(xhr.status === 200){
+          loader.classList.remove('is-active');
           html = xhr.responseText;
           readExcel(html);
-        }else{
+        }else if(xhr.status === 404){
+          loader.classList.remove('is-active');
           error.classList.add('active');
           error.innerHTML = 'Não encontramos nenhuma tabela nessa url';
         }
@@ -46,23 +50,36 @@ function ajaxHtml(){
 function readExcel(cont){
   error.classList.remove('active');
   clip.classList.remove('active');
+  //LIMPANDO O HTML ANTES DE GERAR UM NOVO
+  viewColor.innerHTML = '';
+  templateHTMLCSS.textContent = '';
+  if(viewCSS.children[1]){
+    viewCSS.children[1].remove();
+  }
+  //PEGANDO TODAS AS TR DA TABELA
   table = parser.parseFromString(cont, "text/html").querySelectorAll('tr');
   table.forEach(function(el, i) {
+    //IGNORANDO A PRIMEIRA TR
     if( i != 0 ){
       className = el.children[1].textContent.replace(' ','-');
       classValue = el.children[2].textContent;
       if(classValue.match(/#/g)){
+        //AQUI MONTA O BACKGROUND-COLOR
         cssColor = '.skuespec_Cores_opcao_' + className + '{ background-color: ' + classValue + '} <br />';            
         viewHtml = '<li><div class="color-thumb" style="background-color: '+ classValue +' "></div><span>'+ className +'</span></li>';
       }else{
+        //AQUI MONTA O BACKGROUND-IMAGE
         cssColor = '.skuespec_Cores_opcao_' + className + '{ background-image: url(' + classValue + ')} <br />';
         viewHtml = '<li><div class="color-thumb" style="background-image: url('+ classValue +')"></div><span>'+ className +'</span></li>';        
       }
       templateHTMLCSS.insertAdjacentHTML('beforeend', cssColor);
+      //AQUI ESTAMOS MONTANDO A PARTE DE CORES PARA O USUARIO VISUALIZAR
+      //O QUE ESTA SENDO GERADO
       viewColor.insertAdjacentHTML('beforeend', viewHtml);
     }    
   });
-  viewCSS.insertAdjacentHTML('beforeend', templateHTMLCSS.textContent);
+  //MONSTRANDO O CSS GERADO
+  viewCSS.insertAdjacentHTML('beforeend', '<div class="markdown-body">' + templateHTMLCSS.textContent + '</div>');  
 }
 
 var clipboard = new Clipboard('[data-js="clip"]', {
